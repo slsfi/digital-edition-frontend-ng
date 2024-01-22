318c664a0e1c633c5b9fc7283ab41263a1806d07
Sebastian Köhler <74658813+SebastianKohler@users.noreply.github.com>
2024-01-22 13:27:20 +0200

    refactor: reduce code duplication related to markdown content
    - Refactor functions and services related to markdown content.
    - Remove duplicate code by moving functions for getting markdown content to the markdown service.
    - Add pipe for marking HTML safe (bypassing sanitization) in order to separate the getting and parsing of markdown to HTML from the trusting of the HTML.
    - Rename MarkdownContentService MarkdownService.

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,9 +1,8 @@
 import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
-import { SafeHtml } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { Observable, switchMap } from 'rxjs';
 
-import { MarkdownContentService } from '@services/markdown-content.service';
+import { MarkdownService } from '@services/markdown.service';
 
 
 @Component({
@@ -12,10 +11,10 @@ import { MarkdownContentService } from '@services/markdown-content.service';
   styleUrls: ['./about.page.scss']
 })
 export class AboutPage implements OnInit {
-  markdownText$: Observable<SafeHtml>;
+  markdownText$: Observable<string | null>;
 
   constructor(
-    private mdContentService: MarkdownContentService,
+    private mdService: MarkdownService,
     private route: ActivatedRoute,
     @Inject(LOCALE_ID) private activeLocale: string
   ) {}
@@ -23,9 +22,9 @@ export class AboutPage implements OnInit {
   ngOnInit() {
     this.markdownText$ = this.route.params.pipe(
       switchMap(({id}) => {
-        return this.mdContentService.getParsedMdContentFromFileID(
+        return this.mdService.getParsedMdContent(
           this.activeLocale + '-' + id,
-          $localize`:@@About.LoadingError:Sidans innehåll kunde inte laddas.`
+          '<p>' + $localize`:@@About.LoadingError:Sidans innehåll kunde inte laddas.` + '</p>'
         );
       })
     );
--- src/app/services/markdown.service.ts
+++ src/app/services/markdown.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { catchError, map, Observable, of } from 'rxjs';
 import { marked } from 'marked';
 
@@ -10,21 +9,15 @@ import { config } from '@config';
 @Injectable({
   providedIn: 'root',
 })
-export class MarkdownContentService {
+export class MarkdownService {
   private apiURL: string = '';
 
   constructor(
-    private http: HttpClient,
-    private sanitizer: DomSanitizer
+    private http: HttpClient
   ) {
     const apiBaseURL = config.app?.backendBaseURL ?? '';
     const projectName = config.app?.projectNameDB ?? '';
     this.apiURL = apiBaseURL + '/' + projectName;
-  }
-
-  getMdContent(fileID: string): Observable<any> {
-    const endpoint = `${this.apiURL}/md/${fileID}`;
-    return this.http.get(endpoint);
   }
 
   getMenuTree(language: string, rootNodeID: string): Observable<any> {
@@ -46,20 +39,38 @@ export class MarkdownContentService {
     );
   }
 
-  getParsedMdContentFromFileID(fileID: string, errorMessage: string = ''): Observable<SafeHtml> {
+  /**
+   * Get the content of a markdown file from the backend parsed to HTML as a string.
+   * @param fileID ID of the file to get content of.
+   * @param errorMessage optional message to display if the file content can’t be fetched.
+   * @returns HTML as a string or null.
+   */
+  getParsedMdContent(fileID: string, errorMessage: string = ''): Observable<string | null> {
     return this.getMdContent(fileID).pipe(
       map((res: any) => {
-        return this.sanitizer.bypassSecurityTrustHtml(
-          this.parseMd(res.content)
-        );
+        return res.content.trim() ? this.parseMd(res.content) : null;
       }),
       catchError((e: any) => {
         console.error('Error loading markdown content', e);
-        return of(errorMessage);
+        return of(errorMessage || null);
       })
     );
   }
 
+  /**
+   * Get the content of a markdown file from the backend. Prefer using the
+   * method 'getParsedMdContent' instead if you need the markdown parsed into HTML.
+   * @param fileID ID of the file to get content of.
+   * @returns object where the markdown content is in the 'content' property as a string.
+   */
+  getMdContent(fileID: string): Observable<any> {
+    const endpoint = `${this.apiURL}/md/${fileID}`;
+    return this.http.get(endpoint);
+  }
+
+  /**
+   * Parses the given markdown to HTML and returns it as a string.
+   */
   parseMd(md: string): string {
     return marked.parse(md) as string;
   }
